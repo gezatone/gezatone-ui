6f32f33b8a56697afbab3f3f63fcf52d4ab6ee71
Andrey Petukhov <andrew.petukhoff@gmail.com>
2016-10-01 22:53:54 +0300

    Rework module carousel-catalog

---
+++ modules/carousel-catalog/carousel-catalog.js
@@ -0,0 +1,2 @@
+var carouselCatalogElem = document.querySelector('.carousel-catalog');
+var carouselCatalog = new Flickity( carouselCatalogElem, {cellAlign: 'left', wrapAround: true, pageDots: false});
